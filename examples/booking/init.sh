770b7e2200089394d698b420e7ada8e7e3ea160c
Ron Smeral <rsmeral@redhat.com>
2013-11-26 14:55:35 +0100

    mysql args

--- examples/booking/init.sh
+++ examples/booking/init.sh
@@ -1,3 +1,5 @@
+
+
 # clean deployment dir
 rm -rf ~/jbosseap/standalone/deployments/*
 
@@ -5,7 +7,7 @@ rm -rf ~/jbosseap/standalone/deployments/*
 sleep 60
 
 # init db
-mysql -S $OPENSHIFT_MYSQL_DB_SOCKET -u $OPENSHIFT_MYSQL_DB_USER -p $OPENSHIFT_MYSQL_DB_PASSWORD -f seamsoak2 < app-root/data/init.sql
+mysql -f seamsoak2 < app-root/data/init.sql
 
 # restart
 gear restart --all --cart jbosseap-6
